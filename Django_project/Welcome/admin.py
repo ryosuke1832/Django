82a53a80359a7956dc31a9853bb21f8ff89ea772
ryosuke1832 <84903480+ryosuke1832@users.noreply.github.com>
2023-03-28 07:14:30 +0900

    2023/3/27 基本完了

--- Django_project/Welcome/admin.py
+++ Django_project/Welcome/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from Welcome.models import MemberList
+
+admin.site.register(MemberList)
--- Django_project/Welcome/views.py
+++ Django_project/Welcome/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from Welcome.forms import WelcmForm
+from django.http import HttpResponseBadRequest
+from Welcome.models import MemberList
 
 def index(request):
-    params ={
-        'title':'Wellcome',
-        'message':'ご登録受付フォーム',
-        'form':WelcmForm
-
+    data = MemberList.objects.all()
+    params = {
+        'title':'Welcome',
+        'message':'all memberlist',
+        'data':data,
     }
-    if (request.method == 'POST'):
-        params['message'] = '名前' + request.POST['name'] + '<br>メール:' + request.POST['mail'] + '<br>年齢' +request.POST['age']
-        params['form'] = WelcmForm(request.POST)
-    return render(request, 'Welcome/index.html',params)
+    return render(request,'Welcome/index.html',params)
